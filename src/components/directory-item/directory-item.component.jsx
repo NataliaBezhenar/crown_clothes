6b10921873cbf4687e42a1e9073922210b93aafb
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2023-09-25 20:56:40 +0200

    styles fix for directory-item

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,16 +1,21 @@
 import "./directory-item.styles.scss";
 
-const DirectoryItem = ({ id, title, imageUrl }) => (
-  <div key={id} className="directory-container">
-    <div
-      className="background-image"
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    />
-    <div className="directory-body-container">
-      <h2>{title}</h2>
-      <p>Shop Now</p>
+const DirectoryItem = ({ category }) => {
+  const { imageUrl, title } = category;
+  return (
+    <div className="directory-item-container">
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="body">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default DirectoryItem;
