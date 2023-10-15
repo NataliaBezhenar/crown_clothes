a92b1e3e7764398a5f14dec063dc30b6446a3ed7
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2023-10-15 18:00:17 +0200

    moved user logic from context to redux

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,8 +1,4 @@
-import { createContext, useEffect, useReducer } from "react";
-import {
-  onAuthStateChangedListener,
-  createUserDocumentFromAuth,
-} from "../utils/firebase/firebase.utils";
+import { createContext, useReducer } from "react";
 import { createAction } from "../utils/reducer/reducer.utils";
 
 export const UserContext = createContext({
@@ -13,8 +9,6 @@ export const UserContext = createContext({
 export const USER_ACTION_TYPES = { SET_CURRENT_USER: "SET_CURRENT_USER" };
 
 const userReducer = (state, action) => {
-  console.log("dispatched");
-  console.log(action);
   const { type, payload } = action;
 
   switch (type) {
@@ -31,17 +25,11 @@ const INITIAL_STATE = { currentUser: null };
 export const UserProvider = ({ children }) => {
   const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
   console.log(currentUser);
+
   const setCurrentUser = (user) =>
     dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
-  const value = { currentUser, setCurrentUser };
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      user && createUserDocumentFromAuth(user);
-      setCurrentUser(user);
-    });
-    return unsubscribe;
-  }, []);
+  const value = { currentUser, setCurrentUser };
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
