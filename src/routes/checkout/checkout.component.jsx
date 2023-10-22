2e63995a72c7252c698648641ba152ddde87e78d
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2023-10-22 20:27:59 +0200

    migrate from context to redux for Cart

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,5 +1,4 @@
-import { useContext } from "react";
-import { CartContext } from "../../contexts/cart.context.jsx";
+import { useSelector } from "react-redux";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component.jsx";
 import {
   CheckoutContainer,
@@ -7,9 +6,14 @@ import {
   HeaderBlock,
   TotalSpan,
 } from "./checkout.styles";
+import {
+  selectCartItems,
+  selectCartTotal,
+} from "../../store/cart/cart.selector.js";
 
 const Checkout = () => {
-  const { cartItems, cartTotal } = useContext(CartContext);
+  const cartItems = useSelector(selectCartItems);
+  const cartTotal = useSelector(selectCartTotal);
 
   return (
     <CheckoutContainer>
