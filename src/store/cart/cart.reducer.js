c41eba7287f18e304b075b5c5c9737f0b5924a36
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2023-10-22 20:30:41 +0200

    cart reducer edit

--- src/store/cart/cart.reducer.js
+++ src/store/cart/cart.reducer.js
@@ -1,6 +1,6 @@
 import { CART_ACTION_TYPES } from "./cart.types";
 
-export const CART_INITIAL_STATE = {
+const CART_INITIAL_STATE = {
   isCartOpen: false,
   cartItems: [],
 };
@@ -10,9 +10,15 @@ export const cartReducer = (state = CART_INITIAL_STATE, action = {}) => {
 
   switch (type) {
     case CART_ACTION_TYPES.SET_CART_ITEMS:
-      return { ...state, cartItems: payload };
-    case CART_ACTION_TYPES.SET_CART_IS_OPEN:
-      return { ...state, isCartOpen: payload };
+      return {
+        ...state,
+        cartItems: payload,
+      };
+    case CART_ACTION_TYPES.SET_IS_CART_OPEN:
+      return {
+        ...state,
+        isCartOpen: payload,
+      };
     default:
       return state;
   }
