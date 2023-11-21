dee29bd79ee3e7d78d0daee55e38c684a10b1e82
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2023-11-21 22:17:44 +0100

    typing root saga

--- src/store/root-saga.ts
+++ src/store/root-saga.ts
@@ -1,8 +1,8 @@
-import { all, call } from "redux-saga/effects";
+import { all, call } from "typed-redux-saga/macro";
 
 import { userSagas } from "./user/user.saga";
 import { categriesSaga } from "./categories/categories.saga";
 
 export function* rootSaga() {
-  yield all([call(categriesSaga), call(userSagas)]);
+  yield* all([call(categriesSaga), call(userSagas)]);
 }
